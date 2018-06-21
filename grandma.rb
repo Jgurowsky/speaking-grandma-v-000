d168c9e5f7c8bb6e9025269f39b56d0e2ed78414
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-06-21 22:04:06 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
-  elseif phrase
+  elsif phrase
   end
 end
 # Whatever you say to grandma, she should respond with
